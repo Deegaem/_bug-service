983b6c390723bfb7b2f07d7e19d76b16de60c28a
elsadiq <sadig@gmx.ch>
2022-03-17 11:24:12 +0100

    Initial commit

--- src/main/java/org/deegaem/bugservice/BugServiceApplication.java
+++ src/main/java/org/deegaem/bugservice/BugServiceApplication.java
@@ -2,12 +2,20 @@ package org.deegaem.bugservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BugServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BugServiceApplication.class, args);
+	}
+	@LoadBalanced
+	public WebClient.Builder loadBalancedWebClientBuilder() {
+		return WebClient.builder();
 	}
 
 }
